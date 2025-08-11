ab5d77f947d27fe63348644faed848c12a73fd30
Joseph <anthony.joseph67150@gmail.com>
2025-08-11 17:31:29 +0200

    Add Tailwind CSS and allow to use scss

--- assets/app.ts
+++ assets/app.ts
@@ -8,6 +8,6 @@ registerReactControllerComponents(require.context('./react/controllers', true, /
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ Encore
      * ENTRY CONFIG
      *
      * Each entry will result in one JavaScript file (e.g. app.ts)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.ts')
 
@@ -58,14 +58,16 @@ Encore
         config.corejs = '3.38';
     })
 
+    .enablePostCssLoader()
+
     // enables Sass/SCSS support
-    //.enableSassLoader()
+    .enableSassLoader()
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
 
     // uncomment if you use React
-    //.enableReactPreset()
+    .enableReactPreset()
 
     // uncomment to get integrity="..." attributes on your script & link tags
     // requires WebpackEncoreBundle 1.4 or higher
