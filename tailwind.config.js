c52fb9193a667f54bf209d945e6cd36ade6e4ade
Joseph <anthony.joseph67150@gmail.com>
2025-08-11 17:31:29 +0200

    Add Tailwind CSS and allow to use scss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      './templates/**/*.twig',
+      './assets/**/*.{js,ts,jsx,tsx,vue,scss,css}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
