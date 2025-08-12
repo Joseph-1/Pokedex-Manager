2bbab5e98a3ace61964e613fbe0abaa3f3855f7b
Joseph <anthony.joseph67150@gmail.com>
2025-08-12 19:02:26 +0200

    Add a Type for Pokemon

--- assets/app.tsx
+++ assets/app.tsx
@@ -1,6 +1,7 @@
 import './bootstrap.js';
 import { registerReactControllerComponents } from '@symfony/ux-react';
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));;
+import './styles/app.scss';
 
 /*
  * Welcome to your app's main JavaScript file!
@@ -8,6 +9,5 @@ registerReactControllerComponents(require.context('./react/controllers', true, /
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.scss';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
