bab1cddf2390f991727b26439008aa48eeb4e956
Joseph <anthony.joseph67150@gmail.com>
2025-08-12 18:41:30 +0200

    Add pokemon CRUD and simple API

---
+++ src/Controller/Api/PokemonController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+final class PokemonController extends AbstractController
+{
+    #[Route('/api/pokemons', name: 'api_pokemon_index', methods: ['GET'])]
+    public function index(PokemonRepository $repo): JsonResponse
+    {
+        $pokemons = $repo->findAll();
+
+        // On mappe les entités vers un tableau "propre" (pas tout l'objet Doctrine)
+        $data = array_map(function ($pokemon) {
+            return [
+                'id' => $pokemon->getId(),
+                'name' => $pokemon->getName(),
+                'pokedexId' => $pokemon->getPokedexId(),
+                'type' => $pokemon->getType(),
+            ];
+        }, $pokemons);
+
+        return $this->json($data);
+    }
+
+
+    #[Route('/api/pokemons/{id}', name: 'api_pokemon_show', methods: ['GET'])]
+    public function show(Pokemon $pokemon = null): JsonResponse
+    {
+        if (!$pokemon) {
+            return $this->json(['error' => 'Pokemon non trouvé'], 404);
+        }
+
+        // On mappe toute les entités vers un tableau propre
+        $data = [
+            'id' => $pokemon->getId(),
+            'name' => $pokemon->getName(),
+            'pokedexId' => $pokemon->getPokedexId(),
+            'type' => $pokemon->getType(),
+            'size' => $pokemon->getSize(),
+            'weight' => $pokemon->getWeight(),
+            'sex' => $pokemon->getSex(),
+        ];
+
+        return $this->json($data);
+    }
+
+
+    #[Route('/api/pokemons', name: 'api_pokemon_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $pokemon = new Pokemon();
+        $pokemon->setName($data['name']);
+        $pokemon->setPokedexId($data['pokedexId']);
+        $pokemon->setType($data['type']);
+        $pokemon->setSize($data['size'] ?? null);
+        $pokemon->setWeight($data['weight'] ?? null);
+        $pokemon->setSex($data['sex'] ?? null);
+
+        $em->persist($pokemon);
+        $em->flush();
+
+        return $this->json(['message' => 'Pokemon created', 'id' => $pokemon->getId()], 201);
+    }
+
+
+    #[Route('/api/pokemons/{id}', name: 'api_pokemon_update', methods: ['PUT'])]
+    public function update(Request $request, EntityManagerInterface $em, Pokemon $pokemon = null): JsonResponse
+    {
+        if (!$pokemon) {
+            return $this->json(['error' => 'Pokemon not found'], 404);
+        }
+
+        $data = json_decode($request->getContent(), true);
+
+        $pokemon->setName($data['name'] ?? $pokemon->getName());
+        $pokemon->setPokedexId($data['pokedexId'] ?? $pokemon->getPokedexId());
+        $pokemon->setType($data['type'] ?? $pokemon->getType());
+        $pokemon->setSize($data['size'] ?? $pokemon->getSize());
+        $pokemon->setWeight($data['weight'] ?? $pokemon->getWeight());
+        $pokemon->setSex($data['sex'] ?? $pokemon->getSex());
+
+        $em->flush();
+
+        return $this->json(['message' => 'Pokemon updated']);
+    }
+
+
+    #[Route('/api/pokemons/{id}', name: 'api_pokemon_delete', methods: ['DELETE'])]
+    public function delete(EntityManagerInterface $em, Pokemon $pokemon = null): JsonResponse
+    {
+        if (!$pokemon) {
+            return $this->json(['error' => 'Pokemon non trouvé'], 404);
+        }
+
+        $em->remove($pokemon);
+        $em->flush();
+
+        return $this->json(['message' => 'Pokemon deleted']);
+    }
+}
