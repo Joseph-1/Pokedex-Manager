fa3e69958291ed03f2841ac72231e2afac52e512
Joseph <anthony.joseph67150@gmail.com>
2025-08-12 21:08:44 +0200

    Modify component PokemonCard

--- assets/js/components/Pokemon/PokemonCard.tsx
+++ assets/js/components/Pokemon/PokemonCard.tsx
@@ -1,23 +1,16 @@
-import React from "react";
-import { Pokemon } from "../../../types/Pokemon";
+import React from 'react';
+import type { Pokemon } from '../../../types/Pokemon';
 
-type PokemonCardProps = {
-    pokemon: Pokemon;
-    onClick?: () => void; // Pour étape 6
+type Props = {
+    pokemon: Pokemon; // le composant attend un objet Pokemon en prop
 };
 
-export default function PokemonCard({ pokemon, onClick }: PokemonCardProps) {
+export default function PokemonCard({ pokemon }: Props) {
     return (
-        <div
-            className="bg-white rounded-xl shadow-lg p-4 flex flex-col items-center w-60 hover:shadow-2xl cursor-pointer transition-shadow duration-300"
-            onClick={onClick}
-        >
-            <h2 className="text-xl font-bold text-gray-800">{pokemon.name}</h2>
-            <p className="text-gray-500">#{pokemon.pokedexId}</p>
+        <div className="pokemon-card">
             <img src={pokemon.imgSrc} alt={pokemon.name} />
-            <span className="mt-2 px-3 py-1 rounded-full text-white bg-blue-500">
-        {pokemon.type}
-      </span>
+            <h3>{pokemon.name}</h3>
+            <p>Type : {pokemon.type}</p>
         </div>
     );
 }
