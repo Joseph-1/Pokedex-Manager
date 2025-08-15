f8accf62847919a4c58a29d0b0bca67dd4b442ae
Joseph <anthony.joseph67150@gmail.com>
2025-08-15 02:35:24 +0200

    feat: add form to create a pokemon

---
+++ migrations/Version20250814235555.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250814235555 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pokemon CHANGE pokedex_id pokedex_id INT DEFAULT NULL, CHANGE size size DOUBLE PRECISION DEFAULT NULL, CHANGE weight weight DOUBLE PRECISION DEFAULT NULL, CHANGE sex sex VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE pokemon CHANGE pokedex_id pokedex_id INT NOT NULL, CHANGE size size DOUBLE PRECISION NOT NULL, CHANGE weight weight DOUBLE PRECISION NOT NULL, CHANGE sex sex VARCHAR(255) NOT NULL');
+    }
+}
