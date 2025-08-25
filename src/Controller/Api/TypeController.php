374a57f55417b8f0e708e220d197545de1a97abc
Joseph <anthony.joseph67150@gmail.com>
2025-08-25 02:30:10 +0200

    forget to add route in our controller

--- src/Controller/Api/TypeController.php
+++ src/Controller/Api/TypeController.php
@@ -6,9 +6,11 @@ use App\Entity\Type;
 use App\Repository\TypeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
 
 final class TypeController extends AbstractController
 {
+    #[Route('/api/types', name: 'api_type_index', methods: ['GET'])]
     public function index(TypeRepository $repo): JsonResponse
     {
         $types = $repo->findAll();
