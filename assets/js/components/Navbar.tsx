dfe93ef239b98b177931bb8d3b18e5023ec39e58
Joseph <anthony.joseph67150@gmail.com>
2025-09-04 11:28:19 +0200

    Enhance PokemonModal

--- assets/js/components/Navbar.tsx
+++ assets/js/components/Navbar.tsx
@@ -7,13 +7,15 @@ export default function Navbar() {
             <div className="flex items-center space-x-6">
                 <Link
                     to="/"
-                    className="text-gray-800 font-semibold hover:text-indigo-500 hover:translate-y-[-1px] transition-transform transition-colors duration-200"
+                    className="text-gray-800 font-semibold hover:text-indigo-500 hover:translate-y-[-1px]
+                    transition-colors duration-200"
                 >
                     Accueil
                 </Link>
                 <Link
                     to="/ajouter"
-                    className="text-gray-800 font-semibold hover:text-indigo-500 hover:translate-y-[-1px] transition-transform transition-colors duration-200"
+                    className="text-gray-800 font-semibold hover:text-indigo-500 hover:translate-y-[-1px]
+                    transition-colors duration-200"
                 >
                     Ajouter un Pokémon
                 </Link>
